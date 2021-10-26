30c60d4d2dbaf32d1e2d0658c9bb4be8512e3cb0
Christopher Campbell <christytc10@gmail.com>
2021-10-26 15:54:54 +0100

    Remove dead events

--- index.js
+++ index.js
@@ -9,24 +9,12 @@ const io = require("socket.io")(http, {
 
 const port = process.env.PORT || 3001;
 
-const joinedUsers = []
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-  socket.on('joined', user => {
-    console.log('User joined: ', user);
-    joinedUsers.push(user);
-  });
-  socket.on('action', user => {
-    console.log('Action by: ', user);
-  });
-  socket.on('createGame', gameId => {
-    console.log('Create Game: ', gameId);
+  socket.on('joinGame', gameId => {
+    console.log('joinGame: ', gameId);
     socket.join(gameId);
     io.to(gameId).emit("joinedGame", gameId)
   });
